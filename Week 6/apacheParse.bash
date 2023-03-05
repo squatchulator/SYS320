962f755f8de25eaadcb07c5ab7b866470fa33624
Miles Cummings <miles.cummings@mymail.champlain.edu>
2023-03-05 14:09:24 -0800

    Week 6

---
+++ Week 6/apacheParse.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Parse Apache Log
+
+#Read in file
+
+#Arguements using the position, they start at $1
+APACHE_LOG="$1"
+#Check if file exists
+if [[ ]]
+then
+	echo "Please specify the path to a log file."
+	exit 1
+fi
